0591e9c9b3faa4647c33245925ea42e28b42b36d
rachel-araujo <rachelaraujo17@gmail.com>
2021-05-09 19:41:05 -0300

    WhatsLab

---
+++ whatslab/src/Components/BotaoEnviar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default class BotaoEnviar extends React.Component {
+render(){
+return(
+    <button onSubmit={this.valorUsuario, this.valueMensagem}>ENVIAR</button>
+);
+}
+}
---
+++ whatslab/src/Components/CaixaMensagem.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+export default class CaixaMensagem extends React.Component {
+    render() {
+        return (
+           <main>
+               
+           </main>
+            
+           
+        );
+
+    }
+}
+
+
+
+
+
+
+// export default function CaixaMensagem(props){
+//     return (
+//         <main className="main">
+            
+
+//         </main>
+//     );
+
+// }
+
+
